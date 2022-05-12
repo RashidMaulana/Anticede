cc7a0b5b682a13f0ad44eb4531d3abb2665aa989
Rashid <19523030@students.uii.ac.id>
2022-05-12 10:44:27 +0700

    Add Splash screen and change application logo.

---
+++ Mobile Development/app/src/main/java/com/bangkit/anticede/AboutActivity.kt
@@ -0,0 +1,11 @@
+package com.bangkit.anticede
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
--- Mobile Development/app/src/main/java/com/bangkit/anticede/MainActivity.kt
+++ Mobile Development/app/src/main/java/com/bangkit/anticede/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.bangkit.anticede
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+        actionBar?.hide()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000L)
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+
+
+            finish()
+        }
     }
 }
