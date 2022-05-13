f4492aeccd9b2c6fd8a5cdbe76e513ea3a1521ac
Rashid <19523030@students.uii.ac.id>
2022-05-13 13:40:28 +0700

    Add disclaimer page

--- Mobile Development/app/src/main/java/com/bangkit/anticede/ui/disclaimer/DisclaimerFragment.kt
+++ Mobile Development/app/src/main/java/com/bangkit/anticede/ui/disclaimer/DisclaimerFragment.kt
@@ -1,12 +1,14 @@
 package com.bangkit.anticede.ui.disclaimer
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.bangkit.anticede.BuildConfig
 import com.bangkit.anticede.databinding.FragmentDisclaimerBinding
 
 class DisclaimerFragment : Fragment() {
@@ -17,26 +19,36 @@ class DisclaimerFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(DisclaimerViewModel::class.java)
 
         _binding = FragmentDisclaimerBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.button2.setOnClickListener { view ->
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // only email apps should handle this
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(DEV_EMAIL))
+                putExtra(Intent.EXTRA_SUBJECT, "Anticede Data Disclaimer Request Remove")
+                putExtra(Intent.EXTRA_TEXT, "Mohon hapus data saya dari aplikasi Anticede")
+            }
+            val packageManager = requireContext().packageManager
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
         }
+        val root: View = binding.root
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object{
+        private const val DEV_EMAIL = BuildConfig.DEVELOPER_EMAIL
     }
 }
