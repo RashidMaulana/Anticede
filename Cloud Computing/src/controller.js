1373d380e3ee489526f270b0b29f006a0d32ba48
tsanva <vasantpns@gmail.com>
2022-05-29 20:18:32 +0700

    done speech-to-text

--- Cloud Computing/src/controller.js
+++ Cloud Computing/src/controller.js
@@ -3,8 +3,7 @@ const { FFMPEGRequest } = require('cute-ffmpeg');
 const { nanoid } = require('nanoid');
 const { Storage } = require('@google-cloud/storage');
 const { existsSync } = require('fs');
-// const fs = require('fs');
-// const path = require('path');
+const speech = require('@google-cloud/speech');
 
 const uploadController = (req, res) => {
     const { file } = req;
@@ -13,11 +12,12 @@ const uploadController = (req, res) => {
     const processedFile = `${nanoid()}.flac`;
     const processedFilePath = `./processed-audio/${processedFile}`;
 
+    // convert from AAC to FLAC
     const ffmpeg = new CuteFFMPEG({
         overwrite: true,
     });
 
-    const request = new FFMPEGRequest({
+    const ffmpegRequest = new FFMPEGRequest({
         input: {
             path: `./uploads/${file.filename}`,
         },
@@ -26,36 +26,65 @@ const uploadController = (req, res) => {
         },
     });
 
-    ffmpeg.convert(request);
-
-    // .then((filePath) => {
-    // // Done
-    // })
-    // .catch((error) => {
-    // // Something went wrong
-    // });
-
-    const storage = new Storage();
-
+    ffmpeg.convert(ffmpegRequest)
+        // .then((filePath) => {
+        //
+        // })
+        .catch((error) => {
+            console.log(`error: ${error}`);
+        });
+    // speech-to-text
+    const speechClient = new speech.SpeechClient();
     const bucketName = 'anticede-speech-test';
+
+    async function speechToText() {
+        // The path to the remote LINEAR16 file
+        const gcsUri = `gs://${bucketName}/audio/${processedFile}`;
+
+        // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+        const audio = {
+            uri: gcsUri,
+        };
+        const config = {
+            encoding: 'FLAC',
+            languageCode: 'id-ID',
+            audioChannelCount: 2,
+        };
+        const speechRequest = {
+            audio,
+            config,
+        };
+
+        // Detects speech in the audio file
+        console.log('start speech to text');
+        const [response] = await speechClient.recognize(speechRequest);
+        const transcription = response.results
+            .map((result) => result.alternatives[0].transcript)
+            .join('\n');
+        console.log(`Transcription: ${transcription}`);
+    }
+
+    // upload to GCS
+    const storage = new Storage();
 
     async function uploadFile() {
         await storage.bucket(bucketName).upload(processedFilePath, {
             destination: `audio/${processedFile}`,
         });
+        await speechToText();
 
         console.log(`${processedFilePath} uploaded successfully to ${bucketName}`);
     }
 
-    const checkTime = 1000;
+    const interval = 1000;
 
-    const timerId = setInterval(() => {
+    const checkLocalFile = setInterval(() => {
         const isExists = existsSync(processedFilePath);
         if (isExists) {
             uploadFile().catch(console.error);
-            clearInterval(timerId);
+            clearInterval(checkLocalFile);
         }
-    }, checkTime);
+    }, interval);
 };
 
 module.exports = uploadController;
