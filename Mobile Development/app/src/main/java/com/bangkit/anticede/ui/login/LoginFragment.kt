25fbe4b7ae68edf0f7fdc91e7e859b76626fc5b7
Rashid <19523030@students.uii.ac.id>
2022-06-07 17:56:28 +0700

    add login page for admin.

--- Mobile Development/app/src/main/java/com/bangkit/anticede/ui/login/LoginFragment.kt
+++ Mobile Development/app/src/main/java/com/bangkit/anticede/ui/login/LoginFragment.kt
@@ -1,10 +1,18 @@
 package com.bangkit.anticede.ui.login
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -17,6 +25,7 @@ import com.bangkit.anticede.databinding.FragmentLoginBinding
 import com.bangkit.anticede.preferences.PreferenceFactory
 import com.bangkit.anticede.preferences.PreferenceViewModel
 import com.bangkit.anticede.preferences.UserPreferences
+import com.bangkit.anticede.ui.admin.login.AdminLoginActivity
 import com.bangkit.anticede.ui.home.HomeViewModel
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
@@ -59,6 +68,8 @@ class LoginFragment : Fragment() {
                 loginViewModel.loginUser(requireContext(), binding.editTextTextUserName.text.toString(), binding.editTextTextPassword.text.toString())
             }
         }
+
+        merubahText()
     }
 
     private fun showLoading(isLoading: Boolean) {
@@ -68,6 +79,36 @@ class LoginFragment : Fragment() {
         } else {
             binding.progressBar3.visibility = View.GONE
         }
+    }
+
+    fun merubahText() {
+
+        //Membuat teksnya
+        val spannableString = SpannableString(getString(R.string.text_login_admin))
+
+        //Menentukan teks dapat diklik dan method apa yang akan dijalankan
+        val clickableSpan = object : ClickableSpan() {
+            //Method yang akan dijalankan saat diklik, mengganti layar activity
+            override fun onClick(view: View) {
+                startActivity(Intent(requireContext(), AdminLoginActivity::class.java))
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                //merubah warna teks yang dapat diklik dan menghilangkan garis bawah
+                super.updateDrawState(ds)
+                ds.color = Color.parseColor("#F52121")
+                ds.isUnderlineText = false
+            }
+
+        }
+        //Mengaplikasikan teks yang dapat diklik
+        spannableString.setSpan(clickableSpan, 19, 24, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+
+        //Mengganti teks pada textview agar dapat diklik
+        binding.toLoginAdmin.text = spannableString
+
+        //merubah movement method
+        binding.toLoginAdmin.movementMethod = LinkMovementMethod.getInstance()
     }
 
 }
