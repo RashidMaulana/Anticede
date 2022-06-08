764322e8d1fa2ee76028f99ab710a3b771a93591
Rio Risqi <risqi.rio63@gmail.com>
2022-06-08 21:23:33 +0700

    Add user list and user delete for admin

--- Mobile Development/app/src/main/java/com/bangkit/anticede/adapter/AdminAdapter.kt
+++ Mobile Development/app/src/main/java/com/bangkit/anticede/adapter/AdminAdapter.kt
@@ -1,46 +1,41 @@
 package com.bangkit.anticede.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bangkit.anticede.api.response.GetAllUserResponseItem
-import com.bangkit.anticede.databinding.ItemRowUserBinding
+import com.bangkit.anticede.R
+import com.bangkit.anticede.model.User
 
-class AdminAdapter(private val listUser: ArrayList<GetAllUserResponseItem>) :
-    RecyclerView.Adapter<AdminAdapter.UserViewHolder>() {
-    private var onItemClickCallback: AdminAdapter.OnItemClickCallback? = null
+class AdminAdapter(private val userList: ArrayList<User>): RecyclerView.Adapter<AdminAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
 
-    inner class UserViewHolder(private val binding: ItemRowUserBinding):
-        RecyclerView.ViewHolder(binding.root){
-        fun bind(getAllUserResponseItem: GetAllUserResponseItem) {
-            binding.root.setOnClickListener {
-                onItemClickCallback?.onItemClicked(getAllUserResponseItem)
-            }
-
-            binding.apply {
-                tvItemName.text = getAllUserResponseItem.username
-                tvItemAge.text = getAllUserResponseItem.age.toString()
-            }
-        }
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var username: TextView = itemView.findViewById(R.id.tv_item_name)
+        var age: TextView = itemView.findViewById(R.id.tv_item_age)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdminAdapter.UserViewHolder {
-        val view = ItemRowUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return UserViewHolder((view))
+    override fun onCreateViewHolder(
+        viewGroup: ViewGroup,
+        viewType: Int
+    ) = ListViewHolder(LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_user, viewGroup, false))
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (username, age) = userList[position]
+        holder.username.text = username
+        holder.age.text = age
+
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(userList[holder.adapterPosition]) }
     }
 
-    override fun onBindViewHolder(holder: AdminAdapter.UserViewHolder, position: Int) {
-        holder.bind(listUser[position])
-    }
-
-    override fun getItemCount() = listUser.size
-
-    interface OnItemClickCallback {
-        fun onItemClicked(data: GetAllUserResponseItem)
-    }
+    override fun getItemCount(): Int = userList.size
 
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallback = onItemClickCallback
     }
 
+    interface OnItemClickCallback {
+        fun onItemClicked(user: User)
+    }
 }
---
+++ Mobile Development/app/src/main/java/com/bangkit/anticede/model/User.kt
@@ -0,0 +1,11 @@
+package com.bangkit.anticede.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    var username: String? = null,
+    var age: String? = null,
+    var id: String? = null
+): Parcelable
